18c51eb4e56c61b6cc093aaa55944aff8a073f2a
hakansarac <hakan.sarac95@gmail.com>
2021-01-12 02:24:45 +0300

    Exercise and Constants classes created and exercises implemented

---
+++ app/src/main/java/com/hakansarac/workoutapp/Constants.kt
@@ -0,0 +1,120 @@
+package com.hakansarac.workoutapp
+
+class Constants {
+    companion object{
+
+        fun defaultExercisesList():ArrayList<Exercise>{
+            val exercisesList = ArrayList<Exercise>()
+            val jumpingJacks = Exercise(
+                1,
+                "Jumping Jacks",
+                R.drawable.ic_jumping_jacks,
+                false,
+                false
+            )
+            exercisesList.add(jumpingJacks)
+
+            val wallSit = Exercise(
+                2,
+                "Wall Sit",
+                R.drawable.ic_wall_sit,
+                false,
+                false
+            )
+            exercisesList.add(wallSit)
+
+            val pushUp = Exercise(
+                3,
+                "Push Up",
+                R.drawable.ic_push_up,
+                false,
+                false
+            )
+            exercisesList.add(pushUp)
+
+            val abdominalCrunch = Exercise(
+                4,
+                "Abdominal Crunch",
+                R.drawable.ic_abdominal_crunch,
+                false,
+                false
+            )
+            exercisesList.add(abdominalCrunch)
+
+            val stepUpOnChair = Exercise(
+                5,
+                "Step-Up onto Chair",
+                R.drawable.ic_step_up_onto_chair,
+                false,
+                false
+                )
+            exercisesList.add(stepUpOnChair)
+
+            val squat = Exercise(
+                6,
+                "Squat",
+                R.drawable.ic_squat,
+                false,
+                false
+            )
+            exercisesList.add(squat)
+
+            val tricepsDipOnChair = Exercise(
+                7,
+                "Triceps Dip On Chair",
+                R.drawable.ic_triceps_dip_on_chair,
+                false,
+                false
+            )
+            exercisesList.add(tricepsDipOnChair)
+
+            val plank = Exercise(
+                8,
+                "Plank",
+                R.drawable.ic_plank,
+                false,
+                false
+            )
+            exercisesList.add(plank)
+
+            val highKneesRunningInPlace = Exercise(
+                9, "High Knees Running In Place",
+                R.drawable.ic_high_knees_running_in_place,
+                false,
+                false
+            )
+            exercisesList.add(highKneesRunningInPlace)
+
+            val lunges = Exercise(
+                10,
+                "Lunges",
+                R.drawable.ic_lunge,
+                false,
+                false
+            )
+            exercisesList.add(lunges)
+
+            val pushUpAndRotation = Exercise(
+                11,
+                "Push up and Rotation",
+                R.drawable.ic_push_up_and_rotation,
+                false,
+                false
+            )
+            exercisesList.add(pushUpAndRotation)
+
+            val sidePlank = Exercise(
+                12,
+                "Side Plank",
+                R.drawable.ic_side_plank,
+                false,
+                false
+            )
+            exercisesList.add(sidePlank)
+
+
+            return exercisesList
+        }
+
+    }
+}
--- app/src/main/java/com/hakansarac/workoutapp/ExerciseActivity.kt
+++ app/src/main/java/com/hakansarac/workoutapp/ExerciseActivity.kt
@@ -3,13 +3,20 @@ package com.hakansarac.workoutapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_exercise.*
 
 class ExerciseActivity : AppCompatActivity() {
 
+    private var exercisesList : ArrayList<Exercise>? = null
+    private var currentExercise = -1
+
     private var restTimer: CountDownTimer? = null
     private var restProgress = 10
+
+    private var exerciseTimer: CountDownTimer? = null
+    private var exerciseProgress = 30
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,30 +31,68 @@ class ExerciseActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+        exercisesList = Constants.defaultExercisesList()
+
         setupRestView()
     }
 
-    private fun setProgressBar(){
-        progressBar.progress = restProgress
+    private fun setRestProgressBar(){
+        progressBarRest.progress = restProgress
         restTimer = object : CountDownTimer(10000,1000){
             override fun onTick(p0: Long) {
-                progressBar.progress = restProgress
+                progressBarRest.progress = restProgress
                 textViewTimer.text = restProgress.toString()
                 restProgress--
             }
 
             override fun onFinish() {
-                //TODO: add exercises here
+                currentExercise++
+                setupExerciseView()
             }
         }.start()
     }
 
+    private fun setExerciseProgressBar(){
+        progressBarExercise.progress = exerciseProgress
+        exerciseTimer = object : CountDownTimer(30000,1000){
+            override fun onTick(p0: Long) {
+                progressBarExercise.progress = exerciseProgress
+                textViewExerciseTimer.text = exerciseProgress.toString()
+                exerciseProgress--
+            }
+
+            override fun onFinish() {
+                if(currentExercise < exercisesList?.size!!-1)
+                    setupRestView()
+                else
+                    Toast.makeText(this@ExerciseActivity,"Congratulations!",Toast.LENGTH_SHORT).show()
+            }
+        }.start()
+    }
+
+
+
     private fun setupRestView(){
+        linearLayoutRestView.visibility = View.VISIBLE
+        linearLayoutExerciseView.visibility = View.GONE
         if(restTimer!= null){
             restTimer!!.cancel()
             restProgress = 10
         }
-        setProgressBar()
+        textViewUpcomingExercise.text = exercisesList!![currentExercise+1].getName()
+        setRestProgressBar()
+    }
+
+    private fun setupExerciseView(){
+        linearLayoutRestView.visibility = View.GONE
+        linearLayoutExerciseView.visibility = View.VISIBLE
+        if(exerciseTimer!= null){
+            exerciseTimer!!.cancel()
+            exerciseProgress = 30
+        }
+        setExerciseProgressBar()
+        imageViewExercise.setImageResource(exercisesList!![currentExercise].getImage())
+        textViewExerciseName.text = exercisesList!![currentExercise].getName()
     }
 
     override fun onDestroy() {
